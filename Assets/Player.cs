3f974b3bc259707ca2bd78650dd655f929626bb5
LAPTOP-EPH67RN9\ASUS <dasithrathnasinghe@gmail.com>
2021-02-12 16:52:30 +0530

    setup playerShip vertical movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
 {
     [Tooltip("ms^-1")][SerializeField]float xSpeed = 4f;
     [Tooltip("ms^-1")] [SerializeField] float xRange = 7f;
+    [Tooltip("ms^-1")] [SerializeField] float ySpeed = 4f;
+    [Tooltip("ms^-1")] [SerializeField] float yMin = -4.35f;
+    [Tooltip("ms^-1")] [SerializeField] float yMax = 4.40f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +20,16 @@ public class Player : MonoBehaviour
     void Update()
     {
         float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float verticalTrow = CrossPlatformInputManager.GetAxis("Vertical");
+
         float xOffsetThisFrame = horizontalThrow * Time.deltaTime*xSpeed;
+        float yOffsetThisframe = verticalTrow * Time.deltaTime * ySpeed;
+
         float rawXPos = transform.localPosition.x + xOffsetThisFrame;
         float clampedXPos=Mathf.Clamp(rawXPos, -xRange, xRange);
-        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+        float rawYPos = transform.localPosition.y + yOffsetThisframe;
+        float clampedYPos = Mathf.Clamp(rawYPos, yMin, yMax);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
 }
