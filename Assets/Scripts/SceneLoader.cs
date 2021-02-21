acd41e88935402246d7f49726d6db6d8fc390f0f
LAPTOP-EPH67RN9\ASUS <dasithrathnasinghe@gmail.com>
2021-02-21 16:24:41 +0530

    reorganized file structure and refactor the scripts

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,23 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
         Invoke("LoadLevelOne", 2f);
     }
 
     void LoadLevelOne()
     {
         SceneManager.LoadScene(1);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
